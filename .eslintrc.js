c32b12c6496c436cdc716294229e1f0e2503511a
Nicole Yu <nicole@Nicoles-Mac-mini.local>
2022-12-31 13:03:33 +1100

    change to composition api

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
   // },
 
   rules: {
-    'no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    'no-unused-vars': process.env.NODE_ENV === 'production' ? 'off' : 'off',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,56 +1,34 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank"
-             rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank"
-             rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a>
-      </li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-    <div>{{sourceData.threads}}}</div>
+    <div v-for="thread in threads" :key="thread.id">
+      <h2>{{ thread.title }}</h2>
+      <div v-for="postid in thread.posts" :key="postid">
+        <p>{{ getPostById(postid).text }}</p>
+        <p>{{ getUserById(getPostById(postid).userId).name }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
+<script setup>
 
 import sourceData from '@/data.json'
+import {reactive} from 'vue' // ref
+// reactive could only be used in arrays or objects
 
-console.log(sourceData)
+const threads = reactive(sourceData.threads)
+const posts = reactive(sourceData.posts)
+const users = reactive(sourceData.users)
 
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  },
-  data() {
-    return {
-      sourceData: sourceData
-    }
-  }
+function getPostById(postId) {
+  return posts.find(p => p.id === postId)
 }
+
+function getUserById(userId) {
+  return users.find(p => p.id === userId)
+}
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
