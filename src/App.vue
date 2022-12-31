11efc1f0bad4f74c639268637c79caaafd8fc706
Nicole Yu <nicole@Nicoles-Mac-mini.local>
2022-12-31 12:49:30 +1100

    add for loop to iterate objects

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="This is Nicole's first Vue app"/>
+  <PageHome></PageHome>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import PageHome from '@/components/PageHome'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    PageHome
   }
 }
 </script>
---
+++ src/components/PageHome.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>Hello from child component</div>
+  <div v-for="thread in threads" :key="thread.id">
+    <div v-for="postId in thread.posts" :key="postId">
+      <p>{{ getPostById(postId).text }}</p>
+      <p>{{ getUserById(getPostById(postId).userId).name }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import sourceData from '@/data.json'
+
+export default {
+  data() {
+    return {
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users
+    }
+  },
+
+  methods: {
+    getPostById(postId) {
+      return this.posts.find(
+        p => p.id === postId
+      )
+    },
+
+    getUserById(userId) {
+      return this.users.find(
+        p => p.id === userId
+      )
+    }
+
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
